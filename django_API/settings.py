5acec2d6899937ae95675465ed9e645b2c98ec44
Stanley Agwu <stanleyagwu2016@gmail.com>
2020-09-08 00:07:34 +0200

    Modified API, keeping secret key in environment variable

--- django_API/settings.py
+++ django_API/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v93(f9mc$vpfs6bxllpc#8!kjay3s8&h6+0)s&s^jknpe#oop2'
+SECRET_KEY = os.environ.get('SECRET_KEY1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
