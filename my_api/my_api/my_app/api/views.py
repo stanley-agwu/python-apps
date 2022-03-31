907cf0189477d40ecbe866fa0f279debe00f84ac
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-03-31 19:17:25 +0200

    created a model and registered to admin

--- my_api/my_api/my_app/api/views.py
+++ my_api/my_api/my_app/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -11,9 +11,10 @@ from rest_framework.views import APIView
 #         }
 #         return Response(content)
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 @api_view()
+@permission_classes([AllowAny])
 def hello_world(request):
     return Response({"message": "Hello, world!"})
