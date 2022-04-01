835736f2d638cd5d233529040047841f7e156113
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-04-01 11:40:59 +0200

    Added create method for models.Viewset

--- my_api/my_api/my_app/api/views.py
+++ my_api/my_api/my_app/api/views.py
@@ -11,7 +11,17 @@ class CarSpecificationViewset(viewsets.ModelViewSet):
 
     def retrieve(self, request, *args, **kwargs):
         params = kwargs
-        cars = CarSpecification.objects.all().filter(car_brand=params['pk'])
+        params_list = params['pk'].split('-')
+        cars = CarSpecification.objects.all().filter(car_brand=params_list[0], car_model=params_list[1])
         serializer = CarSpecificationSerializer(cars, many=True)
 
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        car_data = request.data
+        new_car = CarSpecification.objects.create(car_brand=car_data['car_brand'], car_model=car_data['car_model'],
+                    car_body=car_data['car_body'], production_year=car_data['production_year'], engine_type=car_data['engine_type'])
+
+        new_car.save()
+        serializer = CarSpecificationSerializer(new_car)
         return Response(serializer.data)
